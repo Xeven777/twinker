0f258986862d8a6eeaae235ecadc700049db8d61
Anish <xevenbiswas@gmail.com>
2024-02-05 19:37:50 +0530

    btn left

--- src/components/TweetCard.tsx
+++ src/components/TweetCard.tsx
@@ -1,64 +1,179 @@
-import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+"use client";
+import userpic from "../assets/Upstream-4.png";
+import { toPng } from "html-to-image";
 import { Button } from "@/components/ui/button";
 import {
-  CalendarDaysIcon,
+  BarChart2Icon,
+  Download,
   HeartIcon,
   MessageCircleIcon,
   RepeatIcon,
-  Share2Icon,
 } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState, useRef, useCallback } from "react";
 
-export function TweetCard() {
+interface TweetCardProps {
+  data: any;
+}
+
+export function TweetCard(data: TweetCardProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [fDate, setfDate] = useState("Just Now");
+  const texts =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl id tristique tincidunt, nisl nunc euismod nisi, in tincidunt nisl nunc euismod nisi.";
+  useEffect(() => {
+    console.log(data);
+    const date = new Date(data.data?.creation_date);
+    setfDate(
+      date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+    );
+  }, [data]);
+
+  const clickImage = useCallback(() => {
+    if (ref.current === null) {
+      return;
+    }
+    toPng(ref.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = "Twinker.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [ref]);
+
   return (
-    <div className="border border-gray-300 rounded-lg max-w-md mx-auto my-4 p-4">
-      <div className="flex space-x-4">
-        <Avatar>
-          <AvatarImage alt="@nextjs" src="/placeholder-avatar.jpg" />
-          <AvatarFallback>JP</AvatarFallback>
-        </Avatar>
-        <div className="space-y-1">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-          <div className="flex items-center pt-2">
-            <CalendarDaysIcon className="mr-2 h-4 w-4 opacity-70" />
-            <span className="text-xs text-gray-500 dark:text-gray-400">
-              Joined December 2021
-            </span>
+    <>
+      <div
+        ref={ref}
+        className="relative border overflow-hidden text-wrap border-gray-700 bg-gradient-to-tr from-slate-900 via-slate-950 to-gray-950 shadow-inner shadow-gray-700 rounded-lg mx-auto p-4 mb-32 before:absolute before:w-32 before:h-20 before:right-2 before:bg-cyan-700/90 before:-z-10 before:rounded-full before:blur-2xl hover:before:-top-5 hover:after:-top-16 before:-top-12 z-10 after:absolute after:w-24 after:h-24 after:bg-blue-800/40 after:-z-10 before:duration-500 after:duration-500 before:transition-all after:transition-all after:rounded-full after:blur after:-top-12 after:-right-6 min-w-80 max-w-[500px]"
+      >
+        <div className="flex space-x-4 items-center">
+          {data.data?.user?.profile_pic_url ? (
+            <Image
+              alt="@xeven"
+              src={data.data?.user?.profile_pic_url}
+              className="w-10 h-10 rounded-full"
+              width={48}
+              height={48}
+            />
+          ) : (
+            <Image
+              alt="@xeven"
+              src={userpic}
+              className="w-10 h-10 rounded-full"
+            />
+          )}
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">
+              @{data.data?.user?.username || "twinker"}
+            </h4>
+            <div className="flex items-center pt-1">
+              <span className="text-xs text-gray-500 dark:text-gray-400">
+                {fDate || "Just now"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p className="text-sm">{data.data?.text || texts}</p>
+
+          {data.data?.media_url && (
+            <div className="mt-4 max-h-[550px] overflow-hidden rounded-lg w-full">
+              <Image
+                className="w-full h-auto"
+                src={data.data?.media_url[0]}
+                alt=""
+                width={430}
+                height={200}
+              />
+            </div>
+          )}
+          <div className="mt-6">
+            <Button className="mr-2 group" variant="ghost" size="sm">
+              <HeartIcon className="mr-2 h-4 w-4 text-rose-600 group-hover:scale-125 transition-all group-hover:animate-pulse" />
+              {data.data?.favorite_count >= 0 ? data.data?.favorite_count : 490}
+            </Button>
+            <Button className="mr-2 group" variant="ghost" size="sm">
+              <RepeatIcon className="mr-2 h-4 w-4 text-green-600 group-hover:scale-125 transition-all group-hover:animate-spin" />
+              {data.data?.retweet_count >= 0 ? data.data?.retweet_count : 43}
+            </Button>
+            <Button className="mr-2 group" variant="ghost" size="sm">
+              <MessageCircleIcon className="mr-2 h-4 w-4 text-cyan-600 group-hover:scale-125 transition-all group-hover:animate-pulse" />
+              {data.data?.quote_count >= 0 ? data.data?.quote_count : 21}
+            </Button>
+            <Button className="mr-2 group" variant="ghost" size="sm">
+              <BarChart2Icon className="mr-2 h-4 w-4 text-zinc-400" />
+              {data.data?.views >= 0 ? data.data?.views : 6272}
+            </Button>
           </div>
         </div>
       </div>
-      <div className="mt-4">
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nisl id tristique tincidunt, nisl nunc euismod nisi, in tincidunt nisl
-          nunc euismod nisi.
-        </p>
-        <div className="mt-2">
-          <span className="text-xs text-gray-500 dark:text-gray-400">
-            <CalendarDaysIcon className="mr-2 h-4 w-4 opacity-70" />
-            January 1, 2022
-          </span>
-        </div>
-        <div className="mt-4">
-          <Button className="mr-2" variant="outline">
-            <HeartIcon className="mr-2 h-4 w-4" />
-            100
-          </Button>
-          <Button className="mr-2" variant="outline">
-            <RepeatIcon className="mr-2 h-4 w-4" />
-            10
-          </Button>
-          <Button className="mr-2" variant="outline">
-            <MessageCircleIcon className="mr-2 h-4 w-4" />
-            20
-          </Button>
-          <Button className="mr-2" variant="outline">
-            <Share2Icon className="mr-2 h-4 w-4" />
-          </Button>
-        </div>
-      </div>
-    </div>
+    </>
   );
+}
+
+{
+  /* // binding_values: null
+// ​​
+// bookmark_count: 0
+// ​​
+// community_note: null
+// ​​
+// conversation_id: "1754398287162134752"
+// ​​
+// creation_date: "Mon Feb 05 06:55:21 +0000 2024"
+// ​​
+// expanded_url: "https://twitter.com/xevenbiswas/status/1754398287162134752/photo/1"
+// ​​
+// extended_entities: Object { media: (1) […] }
+// ​​
+// favorite_count: 4
+// ​​
+// in_reply_to_status_id: null
+// ​​
+// language: "en"
+// ​​
+// media_url: Array [ "https://pbs.twimg.com/media/GFjfO1PWwAAMKrW.jpg" ]
+// ​​
+// quote_count: 0
+// ​​
+// quoted_status: null
+// ​​
+// quoted_status_id: null
+// ​​
+// reply_count: 0
+// ​​
+// retweet: false
+// ​​
+// retweet_count: 0
+// ​​
+// retweet_status: null
+// ​​
+// retweet_tweet_id: null
+// ​​
+// source: "Twitter Web App"
+// ​​
+// text: "i have exams tomorrow on python. Why tf am i making projects in next.js ?! 😭😭\n\n#nextjs #vscode #100DaysOfCode #buildinpublic #tech https://t.co/gqxc8ratbm"
+// ​​
+// timestamp: 1707116121
+// ​​
+// tweet_id: "1754398287162134752"
+// ​​
+// user: Object { creation_date: "Thu Apr 06 04:58:20 +0000 2023", user_id: "1643840488502620160", username: "xevenbiswas", … }
+// ​​
+// video_url: null
+// ​​
+// video_view_count: null
+// ​​
+// views: 48 */
 }
--- src/components/TweetForm.tsx
+++ src/components/TweetForm.tsx
@@ -5,20 +5,26 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
 import { TweetCard } from "./TweetCard";
+// import { getId } from "@/actions";
 const TweetForm = () => {
   const [tweet, setTweet] = useState("");
+  const [tweetId, setTweetId] = useState("");
+  const [result, setResult] = useState({});
   const url = "https://twitter154.p.rapidapi.com/tweet/details?tweet_id=";
   const options = {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY || "",
-      "X-RapidAPI-Host": process.env.NEXT_PUBLIC_RAPIDAPI_HOST || "",
+      "X-RapidAPI-Host": "twitter154.p.rapidapi.com",
     },
   };
   async function run() {
+    setTweetId(getId(tweet));
+    console.log(tweetId);
     try {
-      const response = await fetch(`${url}${tweet}`, options);
-      const result = await response.text();
+      const response = await fetch(`${url}${tweetId}`, options);
+      const res = await response.json();
+      setResult(res);
       console.log(result);
     } catch (error) {
       console.error(error);
@@ -28,9 +34,9 @@ const TweetForm = () => {
   return (
     <div>
       <div>
-        <TweetCard />
+        <TweetCard data={result} />
       </div>
-      <div className="z-0 fixed left-1/2 -translate-x-1/2 bottom-6 border rounded-md mt-6 py-6 px-4 md:min-w-72 gap-2 flex items-center justify-center">
+      <div className="z-0 backdrop-blur-md fixed left-1/2 -translate-x-1/2 bottom-6 border rounded-md mt-6 py-6 px-4 md:min-w-72 gap-2 flex items-center justify-center">
         <Input
           type="url"
           required
@@ -50,3 +56,10 @@ const TweetForm = () => {
 };
 
 export default TweetForm;
+
+function getId(tweetUrl: string): string {
+  const url = new URL(tweetUrl);
+  const pathComponents = url.pathname.split('/');
+  const tweetId = pathComponents[pathComponents.length - 1];
+  return tweetId;
+}
